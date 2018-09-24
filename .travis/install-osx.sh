cfcf5caad33b3973d90605517161e9759ec5e08f
njlr <njlr@users.noreply.github.com>
2018-09-24 16:54:54 +0100

    * Tweaks jenv steps

--- .travis/install-osx.sh
+++ .travis/install-osx.sh
@@ -6,8 +6,11 @@ if which jenv > /dev/null; then eval "$(jenv init -)"; fi
 echo 'export PATH="$HOME/.jenv/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(jenv init -)"' >> ~/.bash_profile
 jenv add /Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/
+jenv rehash 
 jenv versions 
 jenv global 1.8
+jenv local 1.8
+jenv shell 1.8
 brew tap facebook/fb
 brew install watchman
 brew install buck
