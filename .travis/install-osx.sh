087a0eb1279fc7a8ccfe1e0e573232500eadf540
njlr <njlr@users.noreply.github.com>
2018-09-24 18:00:26 +0100

    * Improves JVM detection

--- .travis/install-osx.sh
+++ .travis/install-osx.sh
@@ -1,12 +1,18 @@
+# Java
 brew tap caskroom/cask
 brew tap caskroom/versions
 brew cask install java8
+
+#jenv
 brew install jenv
-rm -rf $HOME/.jenv
 if which jenv > /dev/null; then eval "$(jenv init -)"; fi
-jenv add /Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/
+export PATH="$HOME/.jenv/shims:$PATH"
+yes | jenv add /Library/Java/JavaVirtualMachines/*/Contents/Home/
 jenv versions 
 jenv global 1.8
+java -version
+
+# Watchman & Buck
 brew tap facebook/fb
 brew install watchman
 brew install buck
