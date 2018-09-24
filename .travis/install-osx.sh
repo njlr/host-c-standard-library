927e0df01ec944dd1015704a9a251b8f4b28241d
njlr <njlr@users.noreply.github.com>
2018-09-24 17:05:42 +0100

    * Cleans jenv

--- .travis/install-osx.sh
+++ .travis/install-osx.sh
@@ -2,6 +2,7 @@ brew tap caskroom/cask
 brew tap caskroom/versions
 brew cask install java8
 brew install jenv
+rm -rf ~/.jenv
 if which jenv > /dev/null; then eval "$(jenv init -)"; fi
 echo 'export PATH="$HOME/.jenv/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(jenv init -)"' >> ~/.bash_profile
@@ -9,8 +10,6 @@ jenv add /Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/
 jenv rehash 
 jenv versions 
 jenv global 1.8
-jenv local 1.8
-jenv shell 1.8
 brew tap facebook/fb
 brew install watchman
 brew install buck
