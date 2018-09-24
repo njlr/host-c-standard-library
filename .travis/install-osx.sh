7ba09c18338fa9e41ad57e0694e4e424ee017cb0
njlr <njlr@users.noreply.github.com>
2018-09-24 18:17:35 +0100

    * Exports jenv Java path

--- .travis/install-osx.sh
+++ .travis/install-osx.sh
@@ -6,7 +6,9 @@ brew cask install java8
 #jenv
 brew install jenv
 if which jenv > /dev/null; then eval "$(jenv init -)"; fi
-export PATH="$HOME/.jenv/shims:$PATH"
+export PATH="$HOME/.jenv/shims:$PATH" 
+echo 'export PATH="$HOME/.jenv/shims:$PATH"' >> ~/.bashrc
+ls /Library/Java/JavaVirtualMachines/
 yes | jenv add /Library/Java/JavaVirtualMachines/*/Contents/Home/
 jenv versions 
 jenv global 1.8
