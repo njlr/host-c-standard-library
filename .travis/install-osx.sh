14f263f7c32bc07ce3dc8c1598ff3c543deb1ec6
njlr <njlr@users.noreply.github.com>
2018-09-24 16:26:51 +0100

    * Tweaks script

--- .travis/install-osx.sh
+++ .travis/install-osx.sh
@@ -2,8 +2,8 @@ brew tap caskroom/cask
 brew tap caskroom/versions
 brew cask install java8
 brew install jenv
-if which jenv > /dev/null; then eval "$(jenv init -)"; fi
-ls /Library/Java/JavaVirtualMachines/
+echo 'export PATH="$HOME/.jenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(jenv init -)"' >> ~/.bash_profile
 jenv add /Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/
 jenv versions 
 jenv global 1.8
